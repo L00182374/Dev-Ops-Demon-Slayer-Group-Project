14beebfcb12086da8f64194ddeaee4d033a16ade
L00182374 <L00182374@atu.ie>
2025-11-07 17:26:07 +0000

    Implemented pydoc and Ruff as a linter, closes #8

--- projectToTest.py
+++ projectToTest.py
@@ -1,8 +1,16 @@
 #projectToTest
+import pydoc# for automatic documentation generation
 
-def lessThan(a,b):
+def lessThan(a,b):# Function to check if a is less than b
+    '''
+    Parameters: integers a and b
+    Returns: True if a is less than b, False otherwise
+    '''
+
     if a < b:
         return True
     else:
         return False
     
+
+pydoc.writedoc('projectToTest')# This will automatically generate the documentation for this file
--- tests.py
+++ tests.py
@@ -1,13 +1,13 @@
-import unittest
-from projectToTest import lessThan
+import unittest# for unit testing
+from projectToTest import lessThan# import the function to be tested
 
-class TestProjectToTest(unittest.TestCase):
+class TestProjectToTest(unittest.TestCase):# create a test case class
 
-    def test_lessThan(self):
+    def test_lessThan(self):# define a test method
         self.assertTrue(lessThan(1, 2))  # Should return True because 1 < 2
         self.assertFalse(lessThan(2, 1))  # Should return False because 2 is not less than 1
         self.assertFalse(lessThan(1, 1))  # Should return False because 1 is not less than 1
-        self.assertFalse(lessThan('a',5))
+        #self.assertFalse(lessThan('a',5)) # creates an error because 'a' and 5 are of different types
 
-if __name__ == '__main__':
+if __name__ == '__main__':# run the unit tests
     unittest.main()
