cc36e11fbec7b330dee663fd3f3aa8afff19376b
L00182374 <L00182374@atu.ie>
2025-11-07 19:36:57 +0000

    created another test closes #5

--- projectToTest.py
+++ projectToTest.py
@@ -13,4 +13,15 @@ def lessThan(a,b):# Function to check if a is less than b
         return False
     
 
+def greaterThan(a,b):# Function to check if a is greater than b
+    '''
+    Parameters: integers a and b
+    Returns: True if a is greater than b, False otherwise
+    '''
+
+    if a > b:
+        return True
+    else:
+        return False
+
 pydoc.writedoc('projectToTest')# This will automatically generate the documentation for this file
--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 import unittest# for unit testing
 from projectToTest import lessThan# import the function to be tested
+from projectToTest import greaterThan# import the second function to be tested
 
 class TestProjectToTest(unittest.TestCase):# create a test case class
 
@@ -8,6 +9,12 @@ class TestProjectToTest(unittest.TestCase):# create a test case class
         self.assertFalse(lessThan(2, 1))  # Should return False because 2 is not less than 1
         self.assertFalse(lessThan(1, 1))  # Should return False because 1 is not less than 1
         #self.assertFalse(lessThan('a',5)) # creates an error because 'a' and 5 are of different types
+
+    def test_greaterThan(self):# define another test method
+        self.assertTrue(greaterThan(2, 1))  # Should return True because 2 > 1
+        self.assertFalse(greaterThan(1, 2))  # Should return False because 1 is not greater than 2
+        self.assertFalse(greaterThan(1, 1))  # Should return False because 1 is not greater than 1
+        #self.assertFalse(greaterThan('a',5)) # creates an error because 'a' and 5 are of different types
 
 if __name__ == '__main__':# run the unit tests
     unittest.main()
