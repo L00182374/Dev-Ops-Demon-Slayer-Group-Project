3818b6c0942d97e72f98e95a67acd05faa28cd31
L00182374 <L00182374@atu.ie>
2025-12-03 00:18:12 +0000

    update(core): projectToTest.py now raises exceptions/errors when anything other than int is entered. This should aid testing.

--- projectToTest.py
+++ projectToTest.py
@@ -10,8 +10,11 @@ def lessThan(a,b):# Function to check if a is less than b
     '''
     Parameters: integers a and b
     Returns: True if a is less than b, False otherwise
+    Raises TypeError if inputs are of different types than integers
     '''
-
+    if not (isinstance(a, (int, float)) and isinstance(b, (int, float))):# Check for numeric types
+        raise TypeError("lessThan expects numeric inputs")# Raise TypeError for invalid types
+    
     if a < b:
         return True
     else:
@@ -22,7 +25,11 @@ def greaterThan(a,b):# Function to check if a is greater than b
     '''
     Parameters: integers a and b
     Returns: True if a is greater than b, False otherwise
+    Raises TypeError if inputs are of different types than integers
     '''
+
+    if not (isinstance(a, (int)) and isinstance(b, (int))):# Check for numeric types
+        raise TypeError("greaterThan expects numeric inputs")# Raise TypeError for invalid types
 
     if a > b:
         logger.info(f'greaterThan called with a={a}, b={b}, returning True')
@@ -36,7 +43,11 @@ def equalTo(a,b):# Function to check if a is equal to b
     '''
     Parameters: integers a and b
     Returns: True if a is equal to b, False otherwise
+    Raises TypeError if inputs are of different types than integers
     '''
+
+    if not (isinstance(a, (int)) and isinstance(b, (int))):# Check for numeric types
+        raise TypeError("equalTo expects numeric inputs")# Raise TypeError for invalid types
 
     if a == b:
         logger.info(f'equalTo called with a={a}, b={b}, returning True')
@@ -50,7 +61,11 @@ def notEqualTo(a,b):# Function to check if a is not equal to b
     '''
     Parameters: integers a and b
     Returns: True if a is not equal to b, False otherwise
+    Raises TypeError if inputs are of different types than integers
     '''
+
+    if not (isinstance(a, (int)) and isinstance(b, (int))):# Check for numeric types
+        raise TypeError("notEqualTo expects numeric inputs")# Raise TypeError for invalid types
 
     if a != b:
         logger.info(f'notEqualTo called with a={a}, b={b}, returning True')
