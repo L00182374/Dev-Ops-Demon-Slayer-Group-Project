cf85faef726187969c4dba820d323196b2e34744
L00182374 <L00182374@atu.ie>
2025-12-24 23:25:02 +0000

    update(lint): formatted main code with black autoformatter to streamline the process.

--- src/projectToTest.py
+++ src/projectToTest.py
@@ -2,77 +2,95 @@
 import pydoc  # for automatic documentation generation
 import logging  # for logging
 
-logging.basicConfig(filename='projectToTest.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(
+    filename="projectToTest.log",
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+)
 logger = logging.getLogger(__name__)  # Create a logger for this module
-logger.info('Module projectToTest loaded')  # Log that the module has been loaded
+logger.info("Module projectToTest loaded")  # Log that the module has been loaded
 
-def lessThan(a,b):  # Function to check if a is less than b
-    '''
+
+def lessThan(a, b):  # Function to check if a is less than b
+    """
     Parameters: integers a and b
     Returns: True if a is less than b, False otherwise
     Raises TypeError if inputs are of different types than integers
-    '''
-    if not (isinstance(a, (int, float)) and isinstance(b, (int, float))):  # Check for numeric types
-        raise TypeError("lessThan expects numeric inputs")  # Raise TypeError for invalid types
-    
+    """
+    if not (
+        isinstance(a, (int, float)) and isinstance(b, (int, float))
+    ):  # Check for numeric types
+        raise TypeError(
+            "lessThan expects numeric inputs"
+        )  # Raise TypeError for invalid types
+
     if a < b:
         return True
     else:
         return False
-    
 
-def greaterThan(a,b):  # Function to check if a is greater than b
-    '''
+
+def greaterThan(a, b):  # Function to check if a is greater than b
+    """
     Parameters: integers a and b
     Returns: True if a is greater than b, False otherwise
     Raises TypeError if inputs are of different types than integers
-    '''
+    """
 
     if not (isinstance(a, (int)) and isinstance(b, (int))):  # Check for numeric types
-        raise TypeError("greaterThan expects numeric inputs")  # Raise TypeError for invalid types
+        raise TypeError(
+            "greaterThan expects numeric inputs"
+        )  # Raise TypeError for invalid types
 
     if a > b:
-        logger.info(f'greaterThan called with a={a}, b={b}, returning True')
+        logger.info(f"greaterThan called with a={a}, b={b}, returning True")
         return True
     else:
-        logger.info(f'greaterThan called with a={a}, b={b}, returning False')
+        logger.info(f"greaterThan called with a={a}, b={b}, returning False")
         return False
-    
 
-def equalTo(a,b):  # Function to check if a is equal to b
-    '''
+
+def equalTo(a, b):  # Function to check if a is equal to b
+    """
     Parameters: integers a and b
     Returns: True if a is equal to b, False otherwise
     Raises TypeError if inputs are of different types than integers
-    '''
+    """
 
     if not (isinstance(a, (int)) and isinstance(b, (int))):  # Check for numeric types
-        raise TypeError("equalTo expects numeric inputs")  # Raise TypeError for invalid types
+        raise TypeError(
+            "equalTo expects numeric inputs"
+        )  # Raise TypeError for invalid types
 
     if a == b:
-        logger.info(f'equalTo called with a={a}, b={b}, returning True')
+        logger.info(f"equalTo called with a={a}, b={b}, returning True")
         return True
     else:
-        logger.info(f'equalTo called with a={a}, b={b}, returning False')
+        logger.info(f"equalTo called with a={a}, b={b}, returning False")
         return False
-    
 
-def notEqualTo(a,b):  # Function to check if a is not equal to b
-    '''
+
+def notEqualTo(a, b):  # Function to check if a is not equal to b
+    """
     Parameters: integers a and b
     Returns: True if a is not equal to b, False otherwise
     Raises TypeError if inputs are of different types than integers
-    '''
+    """
 
     if not (isinstance(a, (int)) and isinstance(b, (int))):  # Check for numeric types
-        raise TypeError("notEqualTo expects numeric inputs")  # Raise TypeError for invalid types
+        raise TypeError(
+            "notEqualTo expects numeric inputs"
+        )  # Raise TypeError for invalid types
 
     if a != b:
-        logger.info(f'notEqualTo called with a={a}, b={b}, returning True')
+        logger.info(f"notEqualTo called with a={a}, b={b}, returning True")
         return True
     else:
-        logger.info(f'notEqualTo called with a={a}, b={b}, returning False')
+        logger.info(f"notEqualTo called with a={a}, b={b}, returning False")
         return False
 
-pydoc.writedoc('projectToTest')  # This will automatically generate the documentation for this file
-logger.info('Documentation for projectToTest generated')
+
+pydoc.writedoc(
+    "projectToTest"
+)  # This will automatically generate the documentation for this file
+logger.info("Documentation for projectToTest generated")
